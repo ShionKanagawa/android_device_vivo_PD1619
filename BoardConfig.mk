94026e65c7a8b3a4e46bbdec60ad9eb1f986c5d8
Vivek Vilvaraj <vvilvara@codeaurora.org>
2018-07-17 00:05:16 +0530

    PD1619: BoardConfig : Enable 64 bit DRM plugins compilation
    Enable 64 bit compilation for DRM plugins.
    This will generate DRM plugins, HALs & oemcrypto_test in 64 bit
    
    Change-Id: I620016aacfab9281a383bbfa44df9c3f75c7baff

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -264,5 +264,8 @@ TARGET_USES_WCNSS_MAC_ADDR_REV		:= true
 # OTA Assert
 TARGET_OTA_ASSERT_DEVICE := PD1619,PD1619MA
 
+#Enable DRM plugins 64 bit compilation
+TARGET_ENABLE_MEDIADRM_64 := true
+
 # inherit from the proprietary version
 -include vendor/vivo/PD1619/BoardConfigVendor.mk
