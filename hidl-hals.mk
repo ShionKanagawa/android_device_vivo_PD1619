20f20c7f46997c8552594beefa047b5e8bee7c9e
Vitaliy Tomin <highwaystar.ru@gmail.com>
2018-08-05 13:07:21 +0800

    PD1619: Add input group to sensors service
    * import default sensors hidl implementation
    * convert to PD1619 target
    * add input group to .rc file
    * binderize sensor hal
    
    Change-Id: Ifaefa77d194faab93674263ca4d6b144ff46c38c

--- hidl-hals.mk
+++ hidl-hals.mk
@@ -98,8 +98,9 @@ PRODUCT_PACKAGES += \
     android.hardware.renderscript@1.0-impl
 
 # Sensors
-# Use the stock PD1619 service/impl pair from proprietary-files.txt. Vivo's
-# userspace expects the stock SSC registry and initialization path.
+PRODUCT_PACKAGES += \
+    android.hardware.sensors@1.0-impl \
+    android.hardware.sensors@1.0-service.PD1619
 
 # Thermal
 PRODUCT_PACKAGES += \
