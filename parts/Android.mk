8f1c25c2387eda6b62964f9785c1914a65aa6948
Bruno Martins <bgcngm@gmail.com>
2019-09-01 13:05:37 +0100

    PD1619: parts: Migrate to androidx
    Change-Id: Ia741c785c3ca1d6fb3d41ac68d64865731fba129

--- parts/Android.mk
+++ parts/Android.mk
@@ -11,11 +11,8 @@ LOCAL_PRIVATE_PLATFORM_APIS := true
 LOCAL_USE_AAPT2 := true
 
 LOCAL_STATIC_ANDROID_LIBRARIES := \
-    android-support-v14-preference \
-    android-support-v7-appcompat \
-    android-support-v7-palette \
-    android-support-v7-preference \
-    android-support-v7-recyclerview
+    androidx.core_core \
+    androidx.preference_preference
 
 include frameworks/base/packages/SettingsLib/common.mk
 
